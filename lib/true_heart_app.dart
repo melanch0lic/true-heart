1223bc9096a8298f9987ce49c3f257c428597719
melanch0lic <kudza76@mail.ru>
2023-07-02 20:51:31 +0300

    login page and firebase initial setup

--- lib/true_heart_app.dart
+++ lib/true_heart_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:true_heart_app/pages/login_page/login_page.dart';
 
 class TrueHeartApp extends StatelessWidget {
   const TrueHeartApp({super.key});
@@ -8,11 +9,11 @@ class TrueHeartApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'True Heart',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Center(child: Text('True Heart')),
+      home: const LoginPage(),
     );
   }
 }
